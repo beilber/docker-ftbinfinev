fb5cfcdc774f1cca4c84af91754246e4f350e858
beilber <brian.eilber@gmail.com>
2016-04-21 22:21:19 -0400

    Adjust memory settings based off testing

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /server
-java -Xms512m -Xmx4096m -jar forge-1.7.10-10.13.3.1401-1710ls-universal.jar nogui
+java -Xms1024M -Xmx2048M -jar forge-1.7.10-10.13.3.1401-1710ls-universal.jar nogui
