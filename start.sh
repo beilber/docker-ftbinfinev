88d8f56f54ae4fbb91455083a39d0ee503484b4a
beilber <brian.eilber@gmail.com>
2016-04-22 21:17:22 -0400

    Changing Memory to be controlled by ENV

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /server
-java -Xms1024M -Xmx2048M -jar forge-1.7.10-10.13.3.1401-1710ls-universal.jar nogui
+java -Xms${MEM} -Xmx${MEM} -jar forge-1.7.10-10.13.3.1401-1710ls-universal.jar nogui
